d7d7d1904594cab37609bd6298d6c7ac5c9eec63
Bienvenido Saez <bienvenidosaez@gmail.com>
2014-10-24 11:50:58 +0200

    Ejemplos de python

---
+++ 04 Python/ej1.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+
+# Inicializamos las variables
+n1 = 0
+n2 = 0
+
+# Pedimos el primer número por pantalla
+print 'Introduzca el primer número'
+n1 = float(input())
+
+print 'Introduzca el segundo número'
+n2 = float(input())
+
+# Calculamos la media, atención a los paréntesis
+media_aritmetica = (n1+n2)/2
+
+print 'La media aritmética es: ' + str(media_aritmetica)
---
+++ 04 Python/entrada_salida.py
@@ -0,0 +1,20 @@
+persona = {
+	'nombre': '',
+	'edad': 0,
+	'sexo': ''
+}
+
+ramon = persona
+
+print 'Dime tu nombre'
+ramon['nombre'] = input()
+
+print 'Dime tu edad'
+ramon['edad'] = input()
+
+print 'Dime tu sexo'
+ramon['sexo'] = input()
+
+
+print ramon
+print persona
