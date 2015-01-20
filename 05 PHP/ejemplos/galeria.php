340e7e2c9d770392bef33c28fcc4bdccb5156972
Bienvenido Sáez Muelas <bienvenidosaez@gmail.com>
2015-01-20 11:16:00 +0100

    Galería terminada

--- 05 PHP/ejemplos/galeria.php
+++ 05 PHP/ejemplos/galeria.php
@@ -5,4 +5,18 @@
     <p>Realizar un script en php que cargue todas las imágenes de la carpeta img y que casa una de ellas sea un link al archivo de imagen en concreto.</p>
 
 
+<?php
+
+    $contador = 1;
+    // <p><img href="img/1.jpg></p>"
+    while ($contador < 10){ ?>
+        <p><a href="img/<?php print $contador; ?>.jpg"><img src="img/<?php print $contador; ?>.jpg" alt=""></a></p>
+
+<?php
+        $contador++;
+    }//Fin del while
+
+?>
+
+
 <?php include('comunes/footer.php'); ?>
