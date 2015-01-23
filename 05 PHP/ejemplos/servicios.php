1d1f96c08a3a9c77d577fa0e01e94bd803e6e529
Bienvenido Sáez Muelas <bienvenidosaez@gmail.com>
2015-01-23 10:48:37 +0100

    prueba

--- 05 PHP/ejemplos/servicios.php
+++ 05 PHP/ejemplos/servicios.php
@@ -1,6 +1,6 @@
 <?php include('comunes/cabecera.php'); ?>
 
-    <h2>Página de Servicios</h2>
+    <h2>Página de Servicios Nueva</h2>
     <p>Este es el contenido de la página de servicios</p>
 
 <?php include('comunes/footer.php'); ?>
